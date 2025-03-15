adcb6bc06fe88da0018de6fab53a525193f17976
Hijiri <crew.sento0864@gmail.com>
2025-03-16 00:01:03 +0900

    html,css更新 firebaseセットアップ

---
+++ main.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDIiBKj8vf1zz6sVklFS9KCVzGdYwthgSw",
+  authDomain: "daily-report-67039.firebaseapp.com",
+  projectId: "daily-report-67039",
+  storageBucket: "daily-report-67039.firebasestorage.app",
+  messagingSenderId: "238197315969",
+  appId: "1:238197315969:web:94e11679dcce5767a0b080"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
